d46c78efdcfe6bb98f3a34e669efadf6167c7d54
Benjamin White <benjamin.e.white1@gmail.com>
2014-06-24 11:43:13 -0400

    changed yo directive to yoYo to demonstrating casing

--- demo1/app.js
+++ demo1/app.js
@@ -1,9 +1,8 @@
 var app = angular.module('myApp', []);
 
-app.directive('yo', function() {
+app.directive('yoYo', function() {
 	return {
 		template: '<div class="yo"><strong>Yo!</strong></div>',
 		restrict: 'AE',
-		replace: true,
 	}
 });
