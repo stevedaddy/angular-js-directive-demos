a18b06df3e7fc6658300af855fec5e051ed7ffc9
Benjamin White <benjamin.e.white1@gmail.com>
2014-06-26 10:39:47 -0400

    added demo of transclusion

--- demo1/app.js
+++ demo1/app.js
@@ -1,8 +1,16 @@
 var app = angular.module('myApp', []);
 
+// app.directive('yoYo', function() {
+// 	return {
+// 		template: '<div class="yo"><strong>Yo!</strong></div>',
+// 		restrict: 'AE',
+// 	}
+// });
+
 app.directive('yoYo', function() {
-	return {
-		template: '<div class="yo"><strong>Yo!</strong></div>',
-		restrict: 'AE',
-	}
-});
+    return {
+        restrict: 'E',
+        template: '<div class="yo-yo">Yo!</div>',
+        replace: true
+    }
+})
