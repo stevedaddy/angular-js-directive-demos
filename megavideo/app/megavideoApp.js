ddd3f07825628e9ca4883abb2524bbe69d58346b
Benjamin White <benjamin.e.white1@gmail.com>
2014-07-10 11:17:01 -0400

    cleaned up link function

--- megavideo/app/megavideoApp.js
+++ megavideo/app/megavideoApp.js
@@ -5,26 +5,27 @@ angular.module('megaVideoDemo', []).
 			templateUrl: 'mega-video.html',
 			scope: true,
 			link: function(scope, element, attrs) {
-				scope.sources = []
-				function trustSrc(url){
-					return $sce.trustAsResourceUrl(url);
-				}
+				scope.sources = [];
+				
 				// whitelist of video formats accepted
-				var _sourceTypes = {
-					webmSrc: { type: 'video/webm'},
-					mp4Src: { type: 'video/mp4'},
-					oggSrc: { type: 'video/ogg'}
-					// etc...
-				}
-				for (srcType in _sourceTypes) {
-					if (attrs.hasOwnProperty(srcType)) {
-						scope.sources.push(
-							{ type: _sourceTypes[srcType].type, 
-							  src: trustSrc(attrs[srcType])
-							}
-						);
+				function processSources() {
+					var sourceTypes = {
+						webm: { type: 'video/webm'},
+						mp4: { type: 'video/mp4'},
+						ogg: { type: 'video/ogg'}
+						// etc...
+					}
+					for (source in sourceTypes) {
+						if (attrs.hasOwnProperty(source)) {
+							scope.sources.push(
+								{ type: sourceTypes[source].type, 
+								  src: $sce.trustAsResourceUrl(attrs[source])
+								}
+							);
+						}
 					}
 				}
+				processSources();
 			}
 			
 		}
