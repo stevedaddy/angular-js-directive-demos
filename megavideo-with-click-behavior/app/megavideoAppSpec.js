d57ed43b2447b923162ee4fe6fbd49f5e5d0e957
Benjamin White <benjamin.e.white1@gmail.com>
2014-07-15 17:26:39 -0400

    fixed incorrect variable ref

--- megavideo-with-click-behavior/app/megavideoAppSpec.js
+++ megavideo-with-click-behavior/app/megavideoAppSpec.js
@@ -14,7 +14,7 @@ describe('megaVideo', function() {
 		oggSrc = 'https://ia600500.us.archive.org/1/items/Duck_and_Cover/1951_duck_and_cover.ogv';
 		mp4Src = 'https://archive.org/download/Duck_and_Cover/1951_duck_and_cover_512kb.mp4"';
 		
-		var html="";
+		html="";
 		html += "<mega-video ";
 		html += "	width=\"60%\" ";
 		html += "	ogg='" + oggSrc + "'" ;
